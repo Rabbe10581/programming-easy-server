26b9a86e58bddc5def98c4676506464e623bb946
Rabbe <onikmahmudrabbe@gmail.com>
2022-10-28 06:07:45 +0600

    details json added

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 
 const courses = require('./data/categories.json')
+const details = require('./data/course-details.json')
 
 app.get('/', (req, res) => {
     res.send('Programming Courses API running');
 });
 
-app.get('/courses', (req, res) => {
+app.get('/details', (req, res) => {
     res.send(courses)
+})
+
+app.get('/details/:id', (req, res) => {
+    console.log(req.params);
+    const id = req.params.id;
+    const deta = details.find(d => d.id == id)
+    res.send(deta);
 })
 
 app.listen(port, () => {
